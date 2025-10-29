a932ae0fde028a15322e470326d1e84774d7db5e
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-10-29 14:20:16 -0300

    Help Desk ERP

---
+++ src/test/java/com/solar/HelpDesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solar.HelpDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
